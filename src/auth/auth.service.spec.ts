55f3851ec4c6357d7e3a45b1382d385f2b2e5f07
johbra90 <jbrand.projects@gmail.com>
2024-09-06 13:01:40 +0000

    Add Tests and Set up Test environment

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,101 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
+import { UsersService } from '@users/users.service';
+import { JwtModule } from '@nestjs/jwt';
+import { User } from '@entities/user.entity';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let fakeUsersService: Partial<UsersService>;
+
 
   beforeEach(async () => {
+    const users: User[] = [];
+    fakeUsersService = {
+      create: (createUserDto: {
+        name: string;
+        email: string;
+        password: string;
+      }) => {
+        const { name, email, password } = createUserDto;
+        users.push({ id: Math.random() * 9999, name, email, password } as User);
+        return Promise.resolve(users[users.length - 1]);
+      },
+      findByEmail: (email: string) => {
+        const filtered = users.filter((user) => user.email === email);
+        return Promise.resolve(filtered.length > 0 ? filtered[0] : null);
+      },
+    };
+
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [JwtModule.register({ secret: process.env.JWT_SECRET })],
+      providers: [
+        AuthService,
+        { provide: UsersService, useValue: fakeUsersService },
+      ],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
   });
 
-  it('should be defined', () => {
+  it('can create an instance of AuthService', async () => {
     expect(service).toBeDefined();
+  });
+
+  it('Create a new User with salted and hashed password', async () => {
+    const user = await service.signUp({
+      name: 'Erika',
+      email: 'erika@mustermann.de',
+      password: '123456',
+    });
+    expect(user).not.toEqual('123456');
+    const [header, payload, signature] = user.accessToken.split('.');
+    expect(header).toBeDefined();
+    expect(payload).toBeDefined();
+    expect(signature).toBeDefined();
+  });
+
+  it('Throw an error when trying to create a User with an existing email', async () => {
+    await service.signUp({
+      name: 'Erika',
+      email: 'erika@mustermann.de',
+      password: '123456',
+    });
+
+    await expect(
+      service.signUp({
+        name: 'Max',
+        email: 'erika@mustermann.de',
+        password: '123456',
+      }),
+    ).rejects.toThrow('Email in use');
+  });
+
+  it('Throw an error when trying to sign in with an invalid email', async () => {
+    await expect(
+      service.signIn('erika@mustermann.de', '123456'),
+    ).rejects.toThrow('Invalid credentials');
+  });
+
+  it('Throw an error when trying to sign in with an invalid password', async () => {
+    await expect(service.signIn('erika@mustermann', 'abcdef')).rejects.toThrow(
+      'Invalid credentials',
+    );
+  });
+
+  it('Return an access token when signing in with valid credentials', async () => {
+    await service.signUp({
+      name: 'Erika',
+      email: 'erika@mustermann.de',
+      password: '123456',
+    });
+
+    const token = await service.signIn('erika@mustermann.de', '123456');
+
+    expect(token.accessToken).toBeDefined();
+    const [header, payload, signature] = token.accessToken.split('.');
+    expect(header).toBeDefined();
+    expect(payload).toBeDefined();
+    expect(signature).toBeDefined();
   });
 });
--- src/media/media.controller.spec.ts
+++ src/media/media.controller.spec.ts
@@ -1,12 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { MediaController } from './media.controller';
+import { MediaService } from './media.service';
 
 describe('MediaController', () => {
   let controller: MediaController;
+  let fakeMediaService: Partial<MediaService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [MediaController],
+      providers: [{ provide: MediaService, useValue: fakeMediaService }],
     }).compile();
 
     controller = module.get<MediaController>(MediaController);
