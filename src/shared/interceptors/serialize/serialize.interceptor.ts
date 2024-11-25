806136136464d307cfce5795e5fcf6b6665c139d
johbra90 <jbrand.projects@gmail.com>
2024-11-25 15:34:26 +0000

    Enhance SerializeInterceptor to support pagination: integrate Reflector for pagination checks and transform responses to PageBlankDto when pagination is enabled

--- src/shared/interceptors/serialize/serialize.interceptor.ts
+++ src/shared/interceptors/serialize/serialize.interceptor.ts
@@ -5,7 +5,9 @@ import {
   NestInterceptor,
   UseInterceptors,
 } from '@nestjs/common';
-import { plainToInstance } from 'class-transformer';
+import { Reflector } from '@nestjs/core';
+import { PageBlankDto } from '@shared/pagination/page-blank.dto';
+import { plainToClassFromExist, plainToInstance } from 'class-transformer';
 import { map, Observable } from 'rxjs';
 
 interface ClassConstructor {
@@ -14,21 +16,51 @@ interface ClassConstructor {
 
 // Custom decorator to apply the SerializeInterceptor with the provided DTO
 export function Serialize(dto: ClassConstructor) {
-  return UseInterceptors(new SerializeInterceptor(dto));
+  return UseInterceptors(new SerializeInterceptor(dto, new Reflector()));
 }
 
 @Injectable()
 export class SerializeInterceptor implements NestInterceptor {
   // The DTO class to which the response data will be transformed
-  constructor(private dto: any) {}
+  constructor(
+    private dto: any,
+    private reflector: Reflector,
+  ) {}
 
   // Intercept the response and transform it to the specified DTO class
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    return next.handle().pipe(
-      map((data: any) => {
-        // Transform the response data to an instance of the DTO class, excluding extraneous values
-        return plainToInstance(this.dto, data, { excludeExtraneousValues: true });
-      }),
+    // Check if pagination is enabled for the current handler
+    const paginate = this.reflector.get<boolean>(
+      'paginate',
+      context.getHandler(),
     );
+
+    if (paginate) {
+      // If pagination is enabled, transform the response data to a paginated DTO class
+      return next.handle().pipe(
+        map((data: any) => {
+          const transformedData = plainToClassFromExist(
+            new PageBlankDto(this.dto),
+            data,
+            {
+              strategy: 'excludeAll', // Exclude all properties not explicitly exposed
+              excludeExtraneousValues: true, // Exclude properties not defined in the DTO
+            },
+          );
+          return transformedData;
+        }),
+      );
+    } else {
+      // If pagination is not enabled, transform the response data to the specified DTO class
+      return next.handle().pipe(
+        map((data: any) => {
+          // Transform the response data to an instance of the DTO class, excluding extraneous values
+          return plainToInstance(this.dto, data, {
+            strategy: 'excludeAll', // Exclude all properties not explicitly exposed
+            excludeExtraneousValues: true, // Exclude properties not defined in the DTO
+          });
+        }),
+      );
+    }
   }
 }
