f907f6f8cfbd5303d7c19f7a6a47dfb44386aeb2
Johannes_Brand <jbrand.projects@gmail.com>
2025-02-07 19:11:54 +0000

    Refactor TypeORM configuration: update entity and subscriber paths, enable logging, and add migration history table

--- src/database/typeorm.config.ts
+++ src/database/typeorm.config.ts
@@ -1,7 +1,4 @@
-import {
-  DataSource,
-  DataSourceOptions,
-} from 'typeorm';
+import { DataSource, DataSourceOptions } from 'typeorm';
 import { User } from './entities/user.entity';
 import { Album } from './entities/album.entity';
 import { AlbumMedia } from './entities/album_media.entity';
@@ -9,6 +6,10 @@ import { Media } from './entities/media.entity';
 import { MediaVariation } from './entities/media_variation.entity';
 import { MediaSubscriber } from './subscribers/media.subscriber';
 import { AlbumSubscriber } from './subscribers/album.subscriber';
+import { Post } from '@entities/post.entity';
+import { PostMedia } from '@entities/post_media.entity';
+import { Story } from '@entities/story.entity';
+import { Comment } from '@entities/comment.entity';
 
 let typeormConfig = {
   type: process.env.TYPEORM_TYPE,
@@ -16,12 +17,13 @@ let typeormConfig = {
   username: process.env.TYPEORM_USERNAME,
   password: process.env.TYPEORM_PASSWORD,
   synchronize: process.env.TYPEORM_SYNCHRONIZE || false,
-  logging: false,
-  entities: [__dirname + '/../**/*.entity.js'],
+  logging: true,
+  entities: ['dist/**/*.entity.js'],
   autoLoadEntities: true,
-  subscribers: [MediaSubscriber, AlbumSubscriber],
-  //entities: [User, Album, Media, MediaVariation, AlbumMedia],
-  migrations: ['@migrations/*.js'],
+  subscribers: ['dist/**/*.subscriber.js'],
+  //entities: [User, Album, Media, MediaVariation, AlbumMedia, Post, PostMedia, Story, Comment],
+  migrations: ['dist/**/migrations/*.js'],
+  migrationsTableName: 'migration_history',
 };
 
 export default typeormConfig as DataSourceOptions;
